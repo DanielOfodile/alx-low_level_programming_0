5ad163cc6a8530562e22360e7fbe3ed1b6b4b511
Daniel <ofodiledaniel20@gmail.com>
2022-04-07 10:42:31 -0700

    scripts for put, printf, and size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#include <stdio.h>
+
+/*
+ * this program displays Programming is like a multilingual puzzle.
+ * it returns zero if run without errors.
+ */
+
+int main(void)
+{
+	puts("\"Prograamming is like building a multilingual puzzle");
+	return (0);
+}
