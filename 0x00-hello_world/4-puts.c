8413ad44ef694055b42fe5c11f150d1d785033bf
Daniel <ofodiledaniel20@gmail.com>
2022-04-07 11:09:39 -0700

    4, 5, and 6 corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
-  * This program prints: Programming is like a multilingual puzzles.
-  * It returns zero if run without errors.
-  */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
