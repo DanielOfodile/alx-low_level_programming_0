d3a60594ec21cbb6f46afa31665974d469c68c1f
Daniel <ofodiledaniel20@gmail.com>
2022-04-12 14:07:00 -0700

    2-print_alphabet_x10.c betty error fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print-alphabet_x10 - prints alphabet 10 times.
- * 
- * return: void.
+ * print_alphabet_x10 - prints alphabet 10 times.
+ *
+ * Return: void.
  */
 
 void print_alphabet_x10(void)
 {
 	char c;
 	int i= 0;
+
 	while (i < 10)
 	{
 		c = 'a';
