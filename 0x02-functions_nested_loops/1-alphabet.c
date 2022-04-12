3438741c35b89c913e1d942cc56723545106318b
Daniel <ofodiledaniel20@gmail.com>
2022-04-12 13:44:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main	- Entry point.
+ *
+ * Return: 0
+ */
+
+void print-alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
