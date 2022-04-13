04102001d6dc7cc474103e3cfe975c91875769b6
Daniel <ofodiledaniel20@gmail.com>
2022-04-13 12:25:58 -0700

    positive or negative test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests functions thaf check if an integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int 0;
+	positive_or_negative(i);
+
+	return (0);
+}
