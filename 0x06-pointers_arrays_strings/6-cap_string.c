770a4e40f12f03d9ebc040f6ffc0c83919ece829
Daniel <ofodiledaniel20@gmail.com>
2022-04-23 08:56:45 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * cap_string - ...
- * @s: ...
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalised.
  *
- * Return: char value
+ * Return: a pointer to the changed string.
  */
-
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc [] = {32, '\t', '\n', 44, '46', ';', '!', '?', '"', '(', ')', '{', '}'};
+	int index = 0;
 
-	while (s[a])
+	while (str[index])
 	{
-		i = 0;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-		while (i < cspc)
-		{
-			if ((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-
-			i++;
-		}
-
-		a++;
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == 't' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+				str[index] -= 32;
+		index++;
 	}
-
-	return (s);
-}	
+	
+	return (str);
+}
