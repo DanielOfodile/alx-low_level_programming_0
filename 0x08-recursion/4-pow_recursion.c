00bd25183d4e2c5981c40f58f5ede96a031f8968
Daniel <ofodiledaniel20@gmail.com>
2022-04-28 03:10:18 -0700

    computes power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,6 @@
  * _pow_recursion - computes the power of two integers.
  * @x: The base integer.
  * @y: The index integer.
- * @power: Power.
  * Return: Returns the power or -1 if y < 0.
  */
 int _pow_recursion(int x, int y)
@@ -12,12 +11,7 @@ int _pow_recursion(int x, int y)
 	int power;
 
 	power = _pow_recursion(x, y);
-
-	if (y == 0)
-	{
-		return (1);
-	}
-	else if (y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
